cf65ff258b14e4cfa4bf48082caf6dd3a471ca71
Habib usman <habibusman64@gmail.com>
2026-03-30 09:23:36 +0100

    Adding my react card components

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <Card />
+    </div>
+  );
+};
+
+export default App;
